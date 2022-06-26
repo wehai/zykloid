7efdace93655bc7c0013720fe85e0bf189c3ab42
wehai <83722180+wehai@users.noreply.github.com>
2022-06-26 16:21:09 +0200

    ein versuch für versandreihenfolge, siehe function send

--- email.mjs
+++ email.mjs
@@ -4,7 +4,7 @@
 import Datastore from '@seald-io/nedb';
 import nodemailer from 'nodemailer'
 
-const sessionsdb = new Datastore({filename:'db/sessionsdb.db'});
+const sessionsdb = new Datastore({filename:'db/sessionsdb.db'})
 const playersdb = new Datastore({filename:'db/playersdb.db'});
 const submissionsdb = new Datastore({filename:'db/submissionsdb.db'});
 try {
